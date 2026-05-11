75ccf5f8ed6d01f39c33b69100793053db71f0e7
Have you seen durkong? <113077142+whereisdurkong@users.noreply.github.com>
2026-05-11 09:17:02 +0800

    Update redirect path after successful login

--- src/views/auth/login.jsx
+++ src/views/auth/login.jsx
@@ -88,7 +88,7 @@ const Auth = async (e) => {
         
         // Redirect after short delay to show success message
         setTimeout(() => {
-          window.location.replace(`AssetReliabilityMonitoringSystem/dashboard`);
+          window.location.replace(`/dashboard`);
         }, 1000);
       } else {
         // No user found with these credentials
